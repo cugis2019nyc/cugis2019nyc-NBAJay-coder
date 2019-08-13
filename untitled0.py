a0ee10b875115e4ef80fda3cc56a530d5de055c8
NBAJay-coder <54039883+NBAJay-coder@users.noreply.github.com>
2019-08-13 13:43:47 -0400

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 11:45:01 2019
+
+@author: Columbia Girls
+"""
+
+hello
+5*2
+5/2
+print("Jaiden")the
+5-2
+(5*2)
+(5/2)
+(5-2)
+(5*2)
+(10,985*325)
+print(35)
+4+9
+500+900
+
+def add(a,b):
+    add = a+b
+    print(add)
+
+def prime(a,b,c):
+    prime = a+b+c
+    print(prime)
+    prime(9,9,9)
+
+print("the sum of 9,9,9 is 27") 
+
+def aqua(a,b,c):
+    aqua = a*b*c
+    print("the product of a+b+c is", aqua)
+    
+aqua("the product of a+b+C", aqua)
+
+prime(5,6,7)
+
+def prime(a,b):
+    prime = a*b
+    print(prime)
+    
+    prime(4,5)
+
+def aqua(a,b,c):
+    aqua = a+b+c
+    print(aqua)
+    
+aqua(3,3,3)
+
+def inferno(b,c,):
+    inferno = b*c *1/2
+    print("the area of the triangle is", inferno)
+    inferno(6,4)
